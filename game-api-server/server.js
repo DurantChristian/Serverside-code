8543dc6d59f5539be4861a1ba0296f9e65bbb8b3
DurantChristian <cdd7@email.sc.edu>
2025-04-13 15:48:03 -0400

    edited server

--- game-api-server/server.js
+++ game-api-server/server.js
@@ -10,6 +10,7 @@ app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(cors());
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
